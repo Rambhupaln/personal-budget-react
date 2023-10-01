65031ea91aa1031218f8564f611e8923cc6c62f1
Rambhupaln <rnaripe1@uncc.edu>
2023-10-01 17:50:47 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+function Footer() {
+  return (
+    <footer className="bottom" role="contentinfo">
+    <div className="center">
+        All rights reserved &copy; Ram Bhupal
+    </div>
+</footer>
+  );
+}
+
+export default Footer;
